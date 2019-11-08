a24c8335c999657f5e03e7c746ce9dc3cef0c63d
mfaridzia <farid19barcelona@gmail.com>
2019-11-08 16:25:31 +0700

    init project

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Hate JavaScript? Try TypeScript
+
+## How to run?
+- Read how to install TypeScript [here](https://www.typescriptlang.org/)
+- Run this command: **tsc -w** to monitor file changes
+- Go to the build folder & run the command **node filename.js**
+
+_JavaScript is eating the world!_
---
+++ src/basics.ts
@@ -0,0 +1,78 @@
+/* 
+  Basics TypeScript
+*/
+
+// Variables
+let txt: string = "Hello World!";
+
+let num: number = 1;
+console.log(num)
+
+num = 2;
+console.log(num) 
+
+const num2: number = 10;
+console.log(num2)
+
+// Simple Array
+const numbers: string[] = ['10', '20', '30'];
+console.log(numbers);
+
+// tuple, fixed length
+let dataTest: [number, number, string] = [1, 2, 'test'] // error -> dataTest = ['test', 2, 2];
+
+// Object
+let biodata: { name: string; age: number} = {
+  name: 'Fulana',
+  age: 20
+}
+/* errror -> 
+  biodata = {
+    name: 'fulana' 
+  }
+*/
+console.log(biodata);
+
+// optional operator
+let biodata2: { name: string; age?: number} = {
+  name: 'Fulana'
+}
+
+// reuse type using interface
+interface IBiodata {
+  name: string;
+  age: number;
+} 
+
+let biodata3: IBiodata = { name: 'fulan', age: 23 };
+
+// Union & Intersection
+interface hasUsername {
+  name: string
+  username: string
+}
+
+interface hasEmail {
+  name: string
+  email: string
+}
+
+// intersection types
+let bio: hasUsername | hasEmail = Math.random() > 0.2 ?
+  {
+    name: 'Fulana bin fulan',
+    username: 'fulana'
+  } : {
+    name: 'Fulana bin fulan',
+    email: 'fulana@test.com'
+  };
+  console.log(bio.name) // can only acces the name property
+
+  // union types
+  let bio2: hasUsername & hasEmail =  {
+    name: 'Fulana bin fulan',
+    username: 'fulana',
+    email: 'fulana@test.com'
+  }
+  // we can acess anything
+  console.log(bio2.name, bio2.username, bio2.email)
