6abd201bfcbc8f29929cf175b0c33c70ccebdc8e
mfaridzia <farid19barcelona@gmail.com>
2019-12-03 00:54:01 +0700

    feat: add type and interface

--- src/interface-and-type.ts
+++ src/interface-and-type.ts
@@ -1,1 +1,21 @@
-// tba....
+import { hasEmail, hasUsername } from "./basics";
+
+// Type alias
+type NumOrStr = number | string;
+type NameType = { name: string };
+
+// Type: Self referencing types
+type NumberValue = 1 | 2;
+
+// type
+type Account2 = (contact: hasEmail | hasUsername, msg: string) => void;
+
+// Interface can extend from other interface
+export interface HasEmailAndPhone extends hasEmail {
+  phoneNumber: number;
+}
+
+// interface and call/index signature
+interface Account {
+  (contact: hasEmail | hasUsername, msg: string): void;
+}
